5a496a15ab20c54b340ea89b218deecd3a2830d2
Angello Parody <angello_parody@hotmail.com>
2026-04-19 10:35:52 +0200

    Slam Toolbox added, evironment mapped

--- launch/launch.sim.launch.py
+++ launch/launch.sim.launch.py
@@ -1,46 +1,42 @@
 import os
-
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
-
 
 def generate_launch_description():
 
+    package_name = 'maqui_na'
 
-    # Include the robot_state_publisher launch file, provided by our own package#
-
-    package_name='maqui_na'
-
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    world_arg = DeclareLaunchArgument(
+        'world', default_value='', description='Path to world file'
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    rsp = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]),
+        launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+        )]),
+        launch_arguments={'world': LaunchConfiguration('world')}.items()
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
-                        output='screen')
+    spawn_entity = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'my_bot',
+        ],
+        additional_env={'PATH': '/usr/bin:' + os.environ.get('PATH', '')},
+        output='screen'
+    )
 
-
-
-    # Launch them all!
-    return LaunchDescription([
-        rsp,
-        gazebo,
-        spawn_entity,
-    ])
+    return LaunchDescription([world_arg, rsp, gazebo, spawn_entity])
